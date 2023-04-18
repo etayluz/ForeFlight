a346e972f25f3a80ebcd1fcdcd821ec6b63e1d84
Etay Luz <etayluz@gmail.com>
2023-04-17 23:41:14 -0400

    Save work

--- foreflight/WeatherReportVC.swift
+++ foreflight/WeatherReportVC.swift
@@ -10,22 +10,44 @@ typealias JSONDictionary = [String:Any]
 
 class WeatherReportVC: UIViewController {
 
+    @IBOutlet weak var contentSwitch: UISwitch!
     @IBOutlet weak var reportTextView: UITextView!
     var reportDic = [String: Any]()
+    var conditionsString = ""
+    var forcastString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         if let report = reportDic["report"] as? JSONDictionary {
 //            print(report)
             if let conditions = report["conditions"] as? JSONDictionary {
                 print(conditions)
                 let jsonData = try? JSONSerialization.data(withJSONObject: conditions, options: [])
-                let jsonString = String(data: jsonData!, encoding: .utf8)
-                reportTextView.text = jsonString
+                if let jsonString = String(data: jsonData!, encoding: .utf8) {
+                    conditionsString = jsonString
+                }
+            }
+            if let forecast = report["forecast"] as? JSONDictionary {
+                print(forecast)
+                let jsonData = try? JSONSerialization.data(withJSONObject: forecast, options: [])
+                if let jsonString = String(data: jsonData!, encoding: .utf8) {
+                    forcastString = jsonString
+                }
             }
         }
-            
         
+        reportTextView.text = conditionsString
     }
+
+
+    
+    @IBAction func didToggleSwitch(_ sender: Any) {
+        if self.contentSwitch.isOn {
+            reportTextView.text = conditionsString
+        } else {
+            reportTextView.text = forcastString
+        }
+    }
+    
 }
